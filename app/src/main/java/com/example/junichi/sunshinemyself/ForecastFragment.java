4c6fd19ea3408edece72e44b8c49e8d4ab50e514
junichi-hk <junichi-hk@users.noreply.github.com>
2015-09-27 01:31:16 +0800

    Postal Code Param

--- app/src/main/java/com/example/junichi/sunshinemyself/ForecastFragment.java
+++ app/src/main/java/com/example/junichi/sunshinemyself/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.junichi.sunshinemyself;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -50,7 +51,7 @@ public class ForecastFragment extends Fragment {
 
         if(id == R.id.action_refresh){
             FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
-            fetchWeatherTask.execute();
+            fetchWeatherTask.execute("94043");
             return(true);
         }
         return super.onOptionsItemSelected(item);
@@ -81,74 +82,19 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(forecastAdapter);
-//        // These two need to be declared outside the try/catch
-//        // so that they can be closed in the finally block.
-//        HttpURLConnection urlConnection = null;
-//        BufferedReader reader = null;
-//
-//        // Will contain the raw JSON response as a string.
-//        String forecastJsonStr = null;
-//
-//        try {
-//            // Construct the URL for the OpenWeatherMap query
-//            // Possible parameters are avaiable at OWM's forecast API page, at
-//            // http://openweathermap.org/API#forecast
-//            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
-//
-//            // Create the request to OpenWeatherMap, and open the connection
-//            urlConnection = (HttpURLConnection) url.openConnection();
-//            urlConnection.setRequestMethod("GET");
-//            urlConnection.connect();
-//
-//            // Read the input stream into a String
-//            InputStream inputStream = urlConnection.getInputStream();
-//            StringBuffer buffer = new StringBuffer();
-//            if (inputStream == null) {
-//                // Nothing to do.
-//                return null;
-//            }
-//            reader = new BufferedReader(new InputStreamReader(inputStream));
-//
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-//                // But it does make debugging a *lot* easier if you print out the completed
-//                // buffer for debugging.
-//                buffer.append(line + "\n");
-//            }
-//
-//            if (buffer.length() == 0) {
-//                // Stream was empty.  No point in parsing.
-//                return null;
-//            }
-//            forecastJsonStr = buffer.toString();
-//        } catch (IOException e) {
-//            Log.e("PlaceholderFragment", "Error ", e);
-//            // If the code didn't successfully get the weather data, there's no point in attemping
-//            // to parse it.
-//            return null;
-//        } finally{
-//            if (urlConnection != null) {
-//                urlConnection.disconnect();
-//            }
-//            if (reader != null) {
-//                try {
-//                    reader.close();
-//                } catch (final IOException e) {
-//                    Log.e("PlaceholderFragment", "Error closing stream", e);
-//                }
-//            }
-//        }
-////        return inflater.inflate(R.layout.fragment_main, container, false);
+
        return(rootView);
     }
 
-    public class FetchWeatherTask extends AsyncTask<Void, Void, Void>{
+    public class FetchWeatherTask extends AsyncTask<String, Void, Void>{
 
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
         @Override
-        protected Void doInBackground(Void... params){
+        protected Void doInBackground(String... params){
+            if(params.length == 0){
+                return null;
+            }
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
             HttpURLConnection urlConnection = null;
@@ -157,11 +103,32 @@ public class ForecastFragment extends Fragment {
             // Will contain the raw JSON response as a string.
             String forecastJsonStr = null;
 
+            String format = "json";
+            String units = "metric";
+            int numDays = 7;
+
             try {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are avaiable at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+//                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+                final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                final String QUERY_PARAM = "q";
+                final String FORMAT_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String DAYS_PARAM = "cnt";
+
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, params[0])
+                        .appendQueryParameter(FORMAT_PARAM, format)
+                        .appendQueryParameter(UNITS_PARAM, units)
+                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                Log.v(LOG_TAG, "Build URI " + builtUri.toString());
+
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
